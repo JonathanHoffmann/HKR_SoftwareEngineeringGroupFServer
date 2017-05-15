ef458483544771eea381ce527dcf5d77aa69a16f
JonathanHoffmann <jonathan.hoffmann@gmx.net>
2017-05-15 23:18:15 +0200

    Changed Echo Thread to new requirements

--- src/server/EchoThread.java
+++ src/server/EchoThread.java
@@ -2,6 +2,7 @@ package server;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
@@ -14,14 +15,13 @@ public class EchoThread extends Thread {
 
     protected Socket socket;
     private int userNum = 0;
-    private final String path = "";
+    private String path = "";
 
     public EchoThread(Socket clientSocket, int i) {
         this.socket = clientSocket;
         userNum = i;
     }
 
-    @Override
     public void run() {
         InputStream inp = null;
         BufferedReader brinp = null;
@@ -73,6 +73,52 @@ public class EchoThread extends Thread {
                         } else {
                             out.writeBytes("error\r\n");
                             out.flush();
+                        }
+                    } else if (line.equals("save")) {
+                        String stat = Methods.sendDataAndReceive(out, brinp, "listening");//waiting login
+                        if (stat.equals("login")) {
+                            String email = Methods.sendDataAndReceive(out, brinp, "listening");
+                            if (email != null) {
+                                String pass = Methods.sendDataAndReceive(out, brinp, "listening");
+                                if (pass != null) {
+                                    String s = Methods.CheckEmailAndPass(email, pass);
+                                    Methods.sendData(out, s);//send connected or no
+                                    System.out.println(s);
+                                    if (s.contains("connected")) {
+                                        String[] splitedS = s.split("X");
+                                        path = "./localdisk/" + splitedS[1];
+                                        stat = Methods.receiveData(brinp);//textName at 0, length at 1
+                                        String[] statSplited = stat.split("X");//textName at 0, length at 1
+                                        System.out.println("lenght is " + statSplited[1] + " lines");
+                                        String realPath = path + "/" + statSplited[0];
+                                        File file = new File(realPath);
+                                        if (file.exists()) {//request a rename
+                                            System.out.println("the file is already exist on server");
+                                            Methods.sendData(out, "fileexist");
+                                            //do nothing will go and wait new command
+                                        } else {
+                                            stat = Methods.sendDataAndReceive(out, brinp, "listening");
+                                            if (stat.equals("startupload")) {
+                                                Methods.sendData(out, "continue");
+                                                if (Methods.receiveTextFile(path, statSplited[0], socket, Integer.parseInt(statSplited[1]), out, brinp)) {
+                                                    //check the date
+                                                    if (Methods.connectToDB()) {
+                                                        Methods.insertFile(statSplited[0], realPath, email, "9999-12-31 23:59:59", email, "9999-12-31 23:59:59");
+                                                        Methods.insertParticipiant(email, realPath, "ad", "yes");
+                                                        Methods.disConnectDB();
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+
+                                    //download
+                                } else {
+                                    out.writeBytes("error\r\n");
+                                    out.flush();
+                                }
+                            }
+
                         }
                     }
                 }
