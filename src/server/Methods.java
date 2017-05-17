de59f526b4355fbc119584827d8ad2cbffa7f63a
JonathanHoffmann <jonathan.hoffmann@gmx.net>
2017-05-17 13:39:45 +0200

    added all required methods

--- src/server/Methods.java
+++ src/server/Methods.java
@@ -3,7 +3,18 @@ package server;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
+import java.nio.file.FileStore;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -192,7 +203,96 @@ public abstract class Methods {
         return "error";
     }
 
-   
+    public static boolean receiveTextFile(String path, String textName, Socket socket, int length, DataOutputStream out, BufferedReader brinp) throws IOException {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < length + 1; i++) {
+            String s = receiveData(brinp);
+            if (s.equals("endoffile")) {
+                System.out.println("file received");
+                sendData(out, "filereceived");
+                Files.write(Paths.get(path + "/" + textName), list, StandardOpenOption.CREATE_NEW);
+                break;
+            } else {
+                list.add(s + "\n");
+                sendData(out, "continue");
+            }
+        }
+        return list.size() == length;
+    }
+
+    public static boolean receiveTextFileUpdate(String path, String access, Socket socket, int length, DataOutputStream out, BufferedReader brinp) throws IOException {
+        ArrayList<String> list = new ArrayList<>();
+        File file = new File(path);
+        String backupPath = titleWithoutExtension(path) + "_backup.txt";
+
+        if (file.exists()) {
+            File back = new File(backupPath);
+            if (back.exists()) {
+                back.delete();
+            }
+            Files.copy(Paths.get(path), Paths.get(backupPath), StandardCopyOption.COPY_ATTRIBUTES);
+            file.delete();
+        }
+        for (int i = 0; i < length + 1; i++) {
+            String s = receiveData(brinp);
+            if (s.equals("endoffile")) {
+                System.out.println("file received");
+                Files.write(Paths.get(path), list, StandardOpenOption.CREATE_NEW);
+                if (access.equals("ad")) {
+                    String parentPath = file.getParent() + "/administrator backup";
+                    String fileName = titleWithoutExtension(file.getName());
+                    File adbackupDIR = new File(parentPath);
+                    if (!adbackupDIR.exists()) {
+                        adbackupDIR.mkdir();
+                    }
+                    String backupAdPath = parentPath + "/" + fileName;
+                    File adbackupFILE = new File(backupAdPath + "_ad_backup.txt");
+                    File oldAdBackup = new File(backupAdPath + "_ad_backup_old.txt");
+                    if (adbackupFILE.exists()) {
+                        if (oldAdBackup.exists()) {
+                            oldAdBackup.delete();
+                        }
+                        Files.copy(Paths.get(backupAdPath + "_ad_backup.txt"), Paths.get(backupAdPath + "_ad_backup_old.txt"), StandardCopyOption.COPY_ATTRIBUTES);
+                        adbackupFILE.delete();
+                    }
+                    Files.write(Paths.get(parentPath + "/" + fileName + "_ad_backup.txt"), list, StandardOpenOption.CREATE_NEW);
+                }
+                break;
+            } else {
+                list.add(s + "\n");
+            }
+        }
+        return list.size() == length;
+    }
+
+    public static String titleWithoutExtension(String title) {
+        int i = 0;
+        for (i = title.length() - 1; i >= 0; i--) {
+            if (title.charAt(i) == '.') {
+                break;
+            }
+        }
+        return title.substring(0, i);
+    }
+
+    public static List<String> getTheTextFile(String p) throws IOException {
+        File file = new File(p);
+        List<String> list = null;
+        if (file.exists()) {
+            list = Files.readAllLines(Paths.get(p));
+        }
+        return list;
+    }
+
+    private static boolean deleteSQL(String sql, String cond) throws SQLException {
+        PreparedStatement statement = conn.prepareStatement(sql);
+        statement.setString(1, cond);
+        int rowsDeleted = statement.executeUpdate();
+        if (rowsDeleted > 0) {
+            return true;
+        }
+        return false;
+    }
 
     public static String haveAccessTo(String email, String p, String request) {
         try {
@@ -216,6 +316,108 @@ public abstract class Methods {
             System.out.println("haveAccessTo method exception\n" + ex);
         }
         return "no";
+    }
+
+    public static String deleteThisPath(String p, String email) {
+        try {
+            boolean pathExist = false;
+            String sql = "select path,createdby from file ";
+            ResultSet result = selectSQL(sql);
+            while (result.next()) {
+                if (result.getString("path").equals(p)) {
+                    pathExist = true;
+                    if (result.getString("createdby").equals(email)) {
+                        //delete
+                        sql = "DELETE FROM file WHERE path=?";
+                        String sql2 = "DELETE FROM file_has_participiants WHERE path=?";
+                        if (deleteSQL(sql, p) && deleteSQL(sql2, p)) {
+                            File file = new File(p);
+                            if (file.exists()) {
+                                file.delete();
+                                return "deleted";
+                            }
+                        }
+                    } else {
+                        return "error";
+                    }
+                }
+            }
+            if (!pathExist) {
+                return "error";
+            }
+        } catch (SQLException ex) {
+            System.out.println("deleteThisPath method exception\n" + ex);
+        }
+        return "error";
+    }
+
+    public static ArrayList<String> getFilesFor(String email) {//return the paths of the files of the email with the creator of each file and the access
+        try {
+            ArrayList<String> arr = new ArrayList<>();
+            String sql = "select path,access,have_the_last_update from file_has_participiants where email='" + email + "'";
+            ResultSet result = selectSQL(sql);
+            while (result.next()) {
+                String creator = "";
+                String updatedby = "";
+                sql = "select createdby,updatedby from file where path='" + result.getString("path") + "'";
+                ResultSet res = selectSQL(sql);
+                while (res.next()) {
+                    creator = res.getString("createdby");
+                    updatedby = res.getString("updatedby");
+                    System.out.println("created by  " + creator + " updated by" + updatedby);
+                }
+                arr.add(result.getString("path") + "X" + result.getString("access") + "X" + result.getString("have_the_last_update") + "X" + creator + "X" + updatedby);
+                creator = "error reading creator";
+            }
+            return arr;
+        } catch (SQLException ex) {
+            Logger.getLogger(Methods.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public static boolean insertFile(String fileName, String path, String email, String createdTime, String updatedBy, String lastUpdate) {
+        try {
+            String sql = "INSERT INTO file(name,path,createdby,createtime,updatedby,lastupdate) VALUES (?,?,?, ?,?,?)";
+            PreparedStatement Pstatement = updateInsert(sql);
+            Pstatement.setString(1, fileName);
+            Pstatement.setString(2, path);
+            Pstatement.setString(3, email);
+            Pstatement.setString(4, createdTime);
+            Pstatement.setString(5, updatedBy);
+            Pstatement.setString(6, lastUpdate);
+            int rowsInserted = Pstatement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("A new FILE was inserted successfully!");
+                return true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Methods.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+
+        return false;
+    }
+
+    public static boolean insertParticipiant(String email, String path, String access, String haveTheLastUpdate) {
+
+        try {
+            String sql = "INSERT INTO file_has_participiants(email,path,access,have_the_last_update) VALUES (?,?,?,?)";
+            PreparedStatement Pstatement = updateInsert(sql);
+            Pstatement.setString(1, email);
+            Pstatement.setString(2, path);
+            Pstatement.setString(3, access);
+            Pstatement.setString(4, haveTheLastUpdate);
+            int rowsInserted = Pstatement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("A new PARTICIPIANT was inserted successfully!");
+                return true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Methods.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+        return false;
     }
 
     public static String CheckEmail(String email, String pass) throws SQLException {
